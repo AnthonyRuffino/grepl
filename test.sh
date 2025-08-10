ea26bb9d0c4b0329dae20f5fa8c2d42af97f1ca3
Anthony Ruffino <aruffino84@gmail.com>
2025-08-10 09:42:00 +0200

    version 0.0.3

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
-rm -rf /tmp/grepl-test && mkdir -p /tmp/grepl-test && cd /tmp/grepl-test
 git clone git@github.com:AnthonyRuffino/grepl.git
 cd grepl
 npm pack
-cd ..
 npm init -y
-npm i /grepl/grepl-0.1.0.tgz
+npm i grepl-0.0.3.tgz
+node -e "import('grepl').then(m => m.install())"
